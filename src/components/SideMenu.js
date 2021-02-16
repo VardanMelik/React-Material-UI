113edb0c56d708b355347adcd78aeb99fb3abfd4
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-02-17 02:22:56 +0400

    update

---
+++ src/components/SideMenu.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core';
+
+// withStyles & makeStyles
+
+const styles = makeStyles({
+    sideMenu: {
+        display: 'flex',
+        flexDirection: 'column',
+        position: 'absolute',
+        left: '0px',
+        width: '320px',
+        height: '100%',
+        backgroundColor: '#253053'
+    }
+})
+
+
+
+const SideMenu = (props) => {
+
+    const {classes} = props;
+    
+    return (
+        <div className={classes.sideMenu}>
+            SideMenu
+        </div>
+    )
+}
+
+export default withStyles(styles)(SideMenu)
---
+++ timer.js
@@ -0,0 +1,4 @@
+/*
+*  17:02/4:16:27
+https://www.youtube.com/watch?v=m-2_gb_3L7Q&ab_channel=CodAffection
+*/
